7d893b8305a9911eb3b163651f06883c5bf1ac05
Y.C. <x.chenyangfan@gmail.com>
2024-05-14 21:44:25 +0200

    add tilt control

---
+++ modifications/tilt-control-adapter.js
@@ -0,0 +1,66 @@
+const goRight = () => {
+    const keyboardEvent = new Event('keydown')
+    keyboardEvent.which = 39
+
+    document.dispatchEvent(keyboardEvent);
+}
+
+const stopGoRight = () => {
+    const keyboardEvent = new Event('keyup')
+    keyboardEvent.which = 39
+
+    document.dispatchEvent(keyboardEvent);
+}
+
+
+const goLeft = () => {
+    const keyboardEvent = new Event('keydown')
+    keyboardEvent.which = 37
+
+    document.dispatchEvent(keyboardEvent);
+}
+
+const stopGoLeft = () => {
+    const keyboardEvent = new Event('keyup')
+    keyboardEvent.which = 37
+
+    document.dispatchEvent(keyboardEvent);
+}
+
+const clearAction = () => {
+    stopGoRight()
+    stopGoLeft()
+}
+
+const jump = () => {
+    const keyboardEvent = new Event('keydown')
+    keyboardEvent.which = 32
+
+    document.dispatchEvent(keyboardEvent);
+}
+const stopJump = () => {
+    const keyboardUpEvent = new Event('keyup')
+    keyboardUpEvent.which = 32
+
+    document.dispatchEvent(keyboardUpEvent);
+}
+
+const handleOrientation = (event) => {
+    if (event.gamma > 7.5) {
+        goRight()
+        return
+    }
+
+    if (event.gamma < -7.5) {
+        goLeft()
+        return
+    }
+
+    clearAction()
+}
+window.addEventListener("deviceorientation", handleOrientation, true);
+window.addEventListener('mousedown', jump)
+window.addEventListener('touchstart', jump)
+window.addEventListener('mouseup', stopJump)
+window.addEventListener('touchend', stopJump)
+
